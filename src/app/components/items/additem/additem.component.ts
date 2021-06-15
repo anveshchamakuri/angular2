858640f2a3bcf346965af9d053fc1ed26c871d07
anvesh <anveshchamakuri20@gmail.com>
2021-06-15 11:28:21 +0530

    'reactiveform'

--- src/app/components/items/additem/additem.component.ts
+++ src/app/components/items/additem/additem.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
+import { FormControl, FormGroup, FormArray, FormBuilder, ValidatorFn,AbstractControl } from '@angular/forms';
 @Component({
   selector: 'app-additem',
   templateUrl: './additem.component.html',
   styleUrls: ['./additem.component.scss']
 })
 export class AdditemComponent implements OnInit {
-
-  constructor(public rtr:Router) { }
+  form: FormGroup;
+  constructor(public rtr:Router,private fb: FormBuilder) {
+    this.form = this.fb.group({
+      published: true,
+      credentials: this.fb.array([]),
+    });
+   }
 
   ngOnInit(): void {
   }
   logout(): void {
     localStorage.clear();
     this.rtr.navigate(['/auth/login'])
+  }
+  addCreds(): void {
+    const creds = this.form.controls.credentials as FormArray;
+    creds.push(this.fb.group({
+      username: '',
+      password: '',
+    }));
   }
 }
